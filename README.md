885236174ae57fd161169b70f6111c03572e4f9a
Hazem Kawas <hazem.k.4488@gmail.com>
2021-12-07 13:56:19 +0100

    done

---
+++ README.md
@@ -0,0 +1,3 @@
+Canvas 
+
+Training with the teacher 
--- circle.js
+++ circle.js
@@ -1,14 +1,21 @@
 window.addEventListener('load', main);
 
 function main() {
-    let nrOfDots = 6;
+    let rotation = 0;
+    let nrOfDots = 600;
     let multiplier = 2;
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
     setFullscreen(canvas);
     draw(context, nrOfDots, multiplier);
-}
 
+    setInterval(() => {
+        rotation += 0.01;
+        multiplier = 2 + Math.round(rotation);
+    // setFullscreen(canvas);
+    draw(context, nrOfDots, multiplier, rotation);
+}, 20);
+}
 
 /**
  * Will draw a beautiful pattern on the screen
@@ -16,16 +23,18 @@ function main() {
  * @param {Number} nrOfDots 
  * @param {Number} multiplier 
  */
-function draw(context, nrOfDots, multiplier) {
-    const radius = window.innerWidth * 0.4;
+function draw(context, nrOfDots, multiplier, rotation) {
+    const width = context.canvas.width;
+    const height = context.canvas.height;
+    const radius = Math.min(width, height) * 0.4;
 
     // Make sure to clear the rect before we draw new stuff!
-    context.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    context.clearRect(0 , 0, width, height);
     
-    // Draw everyting!
-    // 1. drawCircle(context, radius);
-    // 2. const dots = drawDotsOnCircle(context, radius, nrOfDots);
-    // 3. drawLinesBetweenDots(context, multiplier, dots);
+    // Draw everything!
+    drawCircle(context, radius)
+    const dots = drawDotsOnCircle(context, radius, nrOfDots, rotation);
+    drawLinesBetweenDots(context, multiplier, dots);
 }
 
 /**
@@ -34,7 +43,16 @@ function draw(context, nrOfDots, multiplier) {
  * @param {Number} radius 
  */
 function drawCircle(context, radius) {
-    // TODO: Implement
+    const x = context.canvas.width * .5;
+    const y = context.canvas.height * .5;
+
+    context.strokeStyle = 'transparent';
+    context.lineWidth = 1;
+    context.beginPath();
+    context.arc (x , y, radius, 0, 2 * Math.PI);
+
+    context.stroke();
+    
 }
 
 /**
@@ -44,8 +62,29 @@ function drawCircle(context, radius) {
  * @param {Number} nrOfDots
  * @returns {Array<any>} the positions of all the dots
  */
-function drawDotsOnCircle(context, radius, nrOfDots) {
+function drawDotsOnCircle(context, radius, nrOfDots, rotation) {
     // TODO: Implement
+    const dots = [];
+    const centerX = context.canvas.width * .5;
+    const centerY = context.canvas.height * .5;
+
+    for (let i = 0; i < nrOfDots; i++) {
+        const angle = (Math.PI * 2 / nrOfDots * i) + rotation;
+        const x = centerX + radius * Math.cos(angle);
+        const y = centerY + radius * Math.sin(angle);
+        const dotsRadius = radius * .02;
+        
+        context.fillStyle = "transparent";
+        context.strokeStyle = "transparent";
+        context.beginPath();
+        context.arc (x , y, dotsRadius, 0, 2 * Math.PI);
+        context.fill();
+        context.stroke();
+
+        dots.push( {x, y });
+    }
+    return dots;
+   
 }
 
 /**
@@ -55,7 +94,20 @@ function drawDotsOnCircle(context, radius, nrOfDots) {
  * @param {Array<any>} dots
  */
 function drawLinesBetweenDots(context, multiplier, dots) {
-    // TODO: Implement
+    context.innerWidth = 2;
+
+    for (let i = 0; i < dots.length; i++) {
+        const start = dots[i];
+        const next = dots[(i * multiplier) % dots.length];
+        const hue = 200 + i % 80;
+
+        context.strokeStyle = 'black';
+        context.beginPath();
+        context.moveTo(start.x, start.y);
+        context.lineTo(next.x, next.y)
+        context.stroke();
+    }
+ 
 }
 
 
